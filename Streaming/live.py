4b45633257fcb13e277640bf34722e54407ec963
Alessandro Martins <almarti.martins@gmail.com>
2023-03-29 18:37:53 -0300

    Update live.py

--- Streaming/live.py
+++ Streaming/live.py
@@ -1,29 +1,28 @@
 import cv2
-import numpy as np
 import redis
 
-# Initialize the redis connection
-# Need to install redis-serv on source device
-# after the install, define bind 0.0.0.0 and deefine too your password on requirepass line
-r = redis.Redis(host='your_host', port=6379, password="your_pass")
+r = redis.Redis(host='localhost', port=6379, password="your pass")
 
-# Image size
+cap = cv2.VideoCapture(0)
+
 width = 640
 height = 480
 
-while True:
-    jpg_as_text = r.get('video_feed')
-
-    jpg_buffer = np.frombuffer(jpg_as_text, dtype=np.uint8)
-
-    frame = cv2.imdecode(jpg_buffer, cv2.IMREAD_COLOR)
+while(True):
+    ret, frame = cap.read()
 
     frame = cv2.resize(frame, (width, height))
 
-    # optional
+    encoded, buffer = cv2.imencode('.jpg', frame)
+
+    jpg_as_text = buffer.tobytes()
+
+    r.set('video_feed', jpg_as_text)
+
     cv2.imshow('frame', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+cap.release()
 cv2.destroyAllWindows()
